f5738395a742609b81e194e0428905a6915be254
Evan Tortola <etortola58@gmail.com>
2021-06-04 12:42:05 -0400

    PM Push 1 ET

---
+++ java/src/main/java/com/techelevator/LackingFundsException.java
@@ -0,0 +1,15 @@
+package com.techelevator;
+
+
+public class LackingFundsException extends Exception {
+
+
+    String message = "";
+        public LackingFundsException(String message) {
+            this.message = message;
+        }
+        public String getMessage() {
+            return message;
+        }
+    }
+
---
+++ java/src/main/java/com/techelevator/NotInStockException.java
@@ -0,0 +1,12 @@
+package com.techelevator;
+
+    public class NotInStockException extends Exception {
+        String message = "";
+        public NotInStockException(String message) {
+            this.message = message;
+        }
+        public String getMessage() {
+            return message;
+        }
+    }
+
