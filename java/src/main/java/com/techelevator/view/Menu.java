830d570755becfe7c952c8d8e0c1a63405b298a9
Evan Tortola <etortola58@gmail.com>
2021-06-04 15:35:05 -0400

    PM Push ET

--- java/src/main/java/com/techelevator/view/Menu.java
+++ java/src/main/java/com/techelevator/view/Menu.java
@@ -3,7 +3,10 @@ package com.techelevator.view;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.math.BigDecimal;
 
 public class Menu {
 
@@ -15,17 +18,61 @@ public class Menu {
 		this.in = new Scanner(input);
 	}
 
-	public Object getChoiceFromOptions(Object[] options) {
+	public Object getChoiceFromOptions(Object[] options, String message) {
 		Object choice = null;
 		while (choice == null) {
 			displayMenuOptions(options);
+			System.out.println(message);
 			choice = getChoiceFromUserInput(options);
 		}
 		return choice;
 	}
 
+	public Object getChoiceFromOptions(Object[] options) {
+		return getChoiceFromOptions(options, "");
+
+	}
+
+	public BigDecimal getAmountFromUserInput() {
+
+		Map<String, String> acceptedBills = new HashMap<String, String>();
+		acceptedBills.put("one", "1");
+		acceptedBills.put("five", "5");
+		acceptedBills.put("ten", "10");
+		acceptedBills.put("twenty", "20");
+
+//			String one = "1";
+//			String five = "5";
+//			String ten = "10";
+//			String twenty = "20";
+
+
+		String userInput;
+		out.println();
+		out.println("Please enter only (1, 5, 10, 20)");
+		out.print("Please enter an amount, type [done] when finished >>> $");
+		out.flush();
+
+		userInput = in.nextLine();
+		if ((!userInput.toLowerCase().equals("done"))
+				&& (userInput.equals("one") || userInput.equals("five") || userInput.equals("ten")
+				|| userInput.equals("twenty"))) {
+			try {
+				return new BigDecimal(userInput);
+			} catch (NumberFormatException ex) {
+				out.println("Please enter a valid number: ");
+				out.println();
+				out.flush();
+			}
+
+		}
+		return null;
+	}
+
 	private Object getChoiceFromUserInput(Object[] options) {
 		Object choice = null;
+		out.print("Please choose an option ");
+		out.flush();
 		String userInput = in.nextLine();
 		try {
 			int selectedOption = Integer.valueOf(userInput);
@@ -33,10 +80,11 @@ public class Menu {
 				choice = options[selectedOption - 1];
 			}
 		} catch (NumberFormatException e) {
-			// eat the exception, an error message will be displayed below since choice will be null
+			// eat the exception, an error message will be displayed below since choice will
+			// be null
 		}
 		if (choice == null) {
-			out.println(System.lineSeparator() + "*** " + userInput + " is not a valid option ***" + System.lineSeparator());
+			out.println(userInput + " is not a valid option");
 		}
 		return choice;
 	}
@@ -46,8 +94,9 @@ public class Menu {
 		for (int i = 0; i < options.length; i++) {
 			int optionNum = i + 1;
 			out.println(optionNum + ") " + options[i]);
+
 		}
-		out.print(System.lineSeparator() + "Please choose an option >>> ");
 		out.flush();
+
 	}
 }
